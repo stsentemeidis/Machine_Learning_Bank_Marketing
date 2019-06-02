cfbb82d6b7d075d2aad3ec28fb01cc8fb73f3742
Ashley O'Mahony <ashley.omahony@gmail.com>
2019-06-02 20:37:58 +0200

    Dummy datasets

--- scripts/split_n_preproc.R
+++ scripts/split_n_preproc.R
@@ -30,6 +30,35 @@ bank_train_B_proc <- predict(preProcValues, bank_train_B)
 bank_test_proc <- predict(preProcValues, bank_test)
 
 
+# Dummify the datasets
+dummy_train <- dummyVars(formula = '~.', data = bank_train_A_proc[, !names(bank_train_A_proc) %in% c('y')])
+assign('bank_train_A_proc_dum', as.data.frame(cbind(
+  predict(dummy_train, bank_train_A_proc[, !names(bank_train_A_proc) %in% c('y')]),
+  bank_train_A_proc[, 'y']
+)))
+colnames(bank_train_A_proc_dum) <- c(colnames(bank_train_A_proc_dum)[-length(colnames(bank_train_A_proc_dum))], 'y')
+bank_train_A_proc_dum[,'y'] <- as.factor(bank_train_A_proc_dum[,'y'])
+levels(bank_train_A_proc_dum$y) <- c('No', 'Yes')
+
+dummy_valid <- dummyVars(formula = '~.', data = bank_train_B_proc[, !names(bank_train_B_proc) %in% c('y')])
+assign('bank_train_B_proc_dum', as.data.frame(cbind(
+  predict(dummy_valid, bank_train_B_proc[, !names(bank_train_B_proc) %in% c('y')]),
+  bank_train_B_proc[, 'y']
+)))
+colnames(bank_train_B_proc_dum) <- c(colnames(bank_train_B_proc_dum)[-length(colnames(bank_train_B_proc_dum))], 'y')
+bank_train_B_proc_dum[,'y'] <- as.factor(bank_train_B_proc_dum[,'y'])
+levels(bank_train_B_proc_dum$y) <- c('No', 'Yes')
+
+dummy_test <- dummyVars(formula = '~.', data = bank_test_proc[, !names(bank_test_proc) %in% c('y')])
+assign('bank_test_proc_dum', as.data.frame(cbind(
+  predict(dummy_test, bank_test_proc[, !names(bank_test_proc) %in% c('y')]),
+  bank_test_proc[, 'y']
+)))
+colnames(bank_test_proc_dum) <- c(colnames(bank_test_proc_dum)[-length(colnames(bank_test_proc_dum))], 'y')
+bank_test_proc_dum[,'y'] <- as.factor(bank_test_proc_dum[,'y'])
+levels(bank_test_proc_dum$y) <- c('No', 'Yes')
+
+
 print(paste0(
   ifelse(exists('start_time'), paste0('[', round(
     difftime(Sys.time(), start_time, units = 'mins'), 1
